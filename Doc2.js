afe45e180a187acf44f8c15a82c41e8d728573e1
GouthamSomaraju <mr.goutham00@gmail.com>
2025-03-26 22:06:09 +0530

    first duplicate

--- Doc2.js
+++ Doc2.js
@@ -568,12 +568,7 @@
 let arr=[2, 1, 3, 5, 3, 2]
 let result=[]
 for(i of arr){
-    if(result.includes(i)){
-        result.push(i)
-        break
-    }else{
-        result.push(i)
-    }
+    
 }   
 
 console.log(result); // Output: 3
